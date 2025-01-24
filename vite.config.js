1758d486144732b7c6b168b57dd0869ec9e78e08
mohamedben12369 <mohamedbenh7.com>
2025-01-24 16:54:46 +0100

    fe

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/static.css', // Add this
+                'resources/css/auth.css', // Add this
+                'resources/css/formule.css', // Add this
+                'resources/css/history.css', // Add this
+                'resources/css/gestion.css', // Add this
+                'resources/css/home.css', // Add this
+                'resources/css/rating.css', // Add this
+                'resources/css/static.css', // Add this
+                'resources/css/themes.css', // Add this
+                'resources/css/pagination.css', // Add this
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
 });
+
