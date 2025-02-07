bf1c86cab93759c50e508b9a80364f19e0e4f6ce
Ayoub Farhaoui <ayoubefarhaoui16@gmail.com>
2025-02-07 21:38:57 +0100

    modifié LOGIN

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 
 <div class="container">
-    
+
     <!-- Conteneur Image -->
     <div class="image-container">
         <img src="{{ asset('images/motdepasse.jfif') }}" alt="Login Image">
@@ -8,9 +8,9 @@
 
     <!-- Conteneur Formulaire -->
     <div class="form-container">
-    
+
     <x-auth-layout title="Inscription" :action="route('register')" submitMessage="Inscription">
-    <p >Necouvrez Nos Articles👆 </p>
+    <p >Decouvrez Nos themes👆 </p>
 <x-input name="name" label="Nom complet" />
 <x-input name="email" label="Adresse e-mail" type="email" />
 <x-input name="password" label="Mot de passe" type="password" />
